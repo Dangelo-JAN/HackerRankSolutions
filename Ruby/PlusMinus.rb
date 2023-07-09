7b4c4e188c760c62749f4fd24bfc50d14893fbc2
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2023-07-09 16:33:53 -0400

    Create PlusMinus.rb

---
+++ Ruby/PlusMinus.rb
@@ -0,0 +1,27 @@
+def plusMinus(arr)
+    # Write your code here
+    p = 0
+    n = 0
+    z = 0    
+    arrLength = arr.length().to_f
+    
+    arr.each do |a|
+        if a > 0
+            p += 1
+        elsif a < 0
+            n += 1
+        elsif a == 0
+            z += 1
+        else
+        end
+    end
+
+    pRatio = p / (arrLength)
+    nRatio = n / (arrLength)
+    zRatio = z / (arrLength)
+    
+    puts pRatio.round(6)
+    puts nRatio.round(6)
+    puts zRatio.round(6)
+
+end
